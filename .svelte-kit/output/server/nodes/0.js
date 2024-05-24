de4e14ebf8dbf8e75d4a40642a8e8b259dccc574
Joshua Cole <jcole@suretybonds.com>
2024-05-24 15:19:29 -0500

    ignore build files

--- .svelte-kit/output/server/nodes/0.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 0;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/layout.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/0.ColSJjZ2.js","_app/immutable/chunks/scheduler.C55nVr2J.js","_app/immutable/chunks/index.Bt9MCtE8.js"];
-export const stylesheets = [];
-export const fonts = [];
--- src/routes/product/+page.svelte
+++ src/routes/product/+page.svelte
@@ -11,7 +11,7 @@
     <h3 class="text-darkerblue text-sm ml-4 font-extralight">by Josh Cole</h3>
     <p class="mt-6">What would you like to do as a person who clicks buttons? You read buttons... then <span class="underline decoration-sun font-medium">click</span> them</p>
     <div class="mt-16 flex justify-around">
-      <button class="bg-sun py-4 px-6 rounded-md text-darkblue  capitalize font-medium w-11/12 hover:scale-102 transition-all">post</button>
+      <button class="bg-darkblue py-4 px-6 rounded-md text-white  capitalize font-medium w-11/12 hover:scale-102 transition-all">post</button>
       <!-- <button class="bg-white py-2 px-6 rounded-md text-darkblue border border-darkblue capitalize font-light flex-grow">delete</button>
       <button class="bg-white py-2 px-6 rounded-md text-darkblue border-darkblue border capitalize font-light flex-grow">save</button> -->
     </div>
